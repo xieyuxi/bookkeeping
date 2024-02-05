7e9f5b3d08a87f102aa82033afba61591e9775a9
xieyuxi <rains_xyx@163.com>
2024-02-05 17:36:04 +0800

    project create

---
+++ src/Statement.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+/**
+ * @projectName: bookkeeping
+ * @package: PACKAGE_NAME
+ * @className: Statement
+ * @author: xieyuxi
+ * @description: TODO
+ * @date: 2024/2/5 17:24
+ * @version: 1.0
+ */
+public class Statement {
+    private Date date;
+}
